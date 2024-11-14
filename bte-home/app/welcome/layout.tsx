8977db2d3edb779010492f30883bfeb75541d1de
HezekiahsPhone <hezekiahs.dev@phone>
2024-11-14 15:31:11 +0100

    fixed alias

--- bte-home/app/welcome/layout.tsx
+++ bte-home/app/welcome/layout.tsx
@@ -1,8 +1,7 @@
 // welcome/layout.tsx
-
 import React from 'react';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 interface LayoutProps {
   children: React.ReactNode;
--- bte-home/app/welcome/page.tsx
+++ bte-home/app/welcome/page.tsx
@@ -1,11 +1,11 @@
 // welcome/page.tsx
 
 import React from 'react';
-import HeroSection from '@components/HeroSection';
-import MainSection from '@components/MainSection';
-import AboutUs from '@components/AboutUs';
-import ContactUs from '@components/ContactUs';
-import TellAFriend from '@components/TellAFriend';
+import HeroSection from '@/components/HeroSection';
+import MainSection from '@/components/MainSection';
+import AboutUs from '@/components/AboutUs';
+import ContactUs from '@/components/ContactUs';
+import TellAFriend from '@/components/TellAFriend';
 import Layout from './layout';
 
 const Page: React.FC = () => {
