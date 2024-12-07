f02cb6120e3c2ea7ed6707d4cd900ce6f5894167
HezekiahsDevAndroidStudio <hezekiahs.dev@devlabs.com>
2024-12-07 13:25:39 +0100

    finishing up

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,35 @@
 // welcome/page.tsx
 
-import React from 'react';
-import HeroSection from '@/components/HeroSection';
-import MainSection from '@/components/MainSection';
-import AboutUs from '@/components/AboutUs';
-import ContactUs from '@/components/ContactUs';
-import TellAFriend from '@/components/TellAFriend';
-
+import React from "react";
+import HeroSection from "@/components/HeroSection";
+import MainSection from "@/components/MainSection";
+import AboutUs from "@/components/AboutUs";
+import ContactUs from "@/components/ContactUs";
+import TellAFriend from "@/components/TellAFriend";
 
 const Page: React.FC = () => {
   return (
-    <div className="max-h-full">
-      <HeroSection />
-      <MainSection />
-      <AboutUs />
-      <ContactUs />
-      <TellAFriend />
+    <div>
+      <section id="home">
+        <HeroSection />
+      </section>
+
+      <section id="main">
+        {" "}
+        <MainSection />
+      </section>
+      <section id="about">
+        {" "}
+        <AboutUs />
+      </section>
+      <section id="contact">
+        {" "}
+        <ContactUs />
+      </section>
+      <section id="refer">
+        {" "}
+        <TellAFriend />
+      </section>
     </div>
   );
 };
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,28 +1,35 @@
 import React from "react";
+import Link from "next/link";
 import { faker } from "@faker-js/faker";
 
 export default function HeroSection() {
   const LoremShort = faker.lorem.paragraph(2);
   return (
-    <div className="hero-section hero-section-blue bg-cover bg-center flex flex-col justify-center items-center h-[100vh] md:h-[80] relative">
-      <div className="absolute inset-0 hero-overlay"></div> {/* Overlay for text contrast */}
-      <div className="z-10 px-4 max-w-4xl text-center">
-        <h1 className="text-4xl md:text-6xl font-bold mt-8 mb-8">
-          Welcome to Beliver&apos;s Tech Expo
-        </h1>
-        <p className="text-lg md:text-xl p-4 mb-6">{LoremShort}</p>
-        <a
-          href="#about"
-          className="inline-block bg-color-primary text-white py-3 px-8 rounded-lg text-lg hover:bg-opacity-90 transition"
-        >
-          Explore More
-        </a>
+    <section id="hero">
+      <div className="hero-section hero-section-blue bg-cover bg-center flex flex-col justify-center items-center h-[100vh] md:h-[80] relative">
+        <div className="absolute inset-0 hero-overlay"></div>{" "}
+        {/* Overlay for text contrast */}
+        <div className="z-10 px-4 max-w-4xl text-center">
+          <h1 className="text-4xl md:text-6xl font-bold mt-8 mb-8">
+            Welcome to Beliver&apos;s Tech Expo
+          </h1>
+          <p className="text-lg md:text-xl p-4 mb-6">{LoremShort}</p>
+          <Link
+            href="#about"
+            className="inline-block bg-color-primary text-white py-3 px-8 rounded-lg text-lg hover:bg-opacity-90 transition"
+          >
+            Explore More
+          </Link>
+        </div>
+        <div className="absolute bottom-10 flex justify-center w-full">
+          <Link
+            href="#about"
+            className="text-color-primary text-xl animate-bounce"
+          >
+            ↓ Scroll Down
+          </Link>
+        </div>
       </div>
-      <div className="absolute bottom-10 flex justify-center w-full">
-        <a href="#about" className="text-color-primary text-xl animate-bounce">
-          ↓ Scroll Down
-        </a>
-      </div>
-    </div>
+    </section>
   );
 }
